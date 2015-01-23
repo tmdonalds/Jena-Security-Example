e87e44ae10da34a63bc7898f692e9b16a234cfa3
trevor <tmdonalds@gmail.com>
2015-01-23 15:17:53 -0500

    fixed test and upgraded securityevaluator

--- buildFusekiwar.py
+++ buildFusekiwar.py
@@ -3,19 +3,30 @@ __author__ = 'trevor'
 import os.path
 import shutil
 
-srcfile = '/home/trevor/IdeaProjects/Jena-Security-Parent/fuseki-shiro-web/target/fuseki-shiro-web-1.0-SNAPSHOT.jar'
-dstdir = '/home/trevor/servers/fuseki/jena-fuseki-dist-2.0.0-SNAPSHOT/work/WEB-INF/lib'
+basedir=os.getcwd()
 
-warSrcFile = '/home/trevor/servers/fuseki/jena-fuseki-dist-2.0.0-SNAPSHOT/fuseki.war'
-fusekiWorkDir = '/home/trevor/servers/fuseki/jena-fuseki-dist-2.0.0-SNAPSHOT/work'
+workDir         = basedir + '/build/'
+cleanFusekiWar  = workDir + 'clean/fuseki.war'
+shiroExtJar     = basedir + '/fuseki-shiro-web/target/fuseki-shiro-web-1.0-SNAPSHOT.jar'
+distDir         = basedir + '/dist'
+libDir          = workDir + 'WEB-INF/lib'
 
-assert os.path.exists(srcfile)
-assert os.path.exists(warSrcFile)
+tomcatDir       = '/home/trevor/servers/tomcat/apache-tomcat-7.0.57/webapps'
 
-shutil.copy(srcfile, dstdir)
-shutil.copy(warSrcFile, fusekiWorkDir)
+if(os.path.exists(tomcatDir + '/fuseki.war')) :
+    os.remove(tomcatDir + '/fuseki.war')
 
-for i in os.listdir(dstdir):
+assert os.path.exists(shiroExtJar)
+assert os.path.exists(cleanFusekiWar)
+
+shutil.copy(shiroExtJar, libDir)
+shutil.copy(cleanFusekiWar, workDir)
+
+os.chdir(workDir)
+
+for i in os.listdir(libDir):
     os.system("jar -uf ./fuseki.war WEB-INF/lib/"+i)
 
-os.system("cp ./fuseki.war /home/trevor/servers/tomcat/apache-tomcat-7.0.57/webapps")
+shutil.copy("./fuseki.war",distDir)
+os.system("cp ./fuseki.war "+tomcatDir)
+os.remove(workDir + 'fuseki.war')
